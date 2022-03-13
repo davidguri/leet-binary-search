ec2990902094b7cca7906b3cf58aea3cc034b966
David Guri <Davidguri@yahoo.com>
2022-03-13 17:44:52 +0100

    finished

--- main.py
+++ main.py
@@ -1,4 +1,23 @@
-# receive input array
-# receive target integer
-# filter the array to see if target is included
-# if so, return it's index
+def binary_search(arr, x):
+    low = 0
+    high = len(arr) - 1
+    mid = 0
+    while low <= high:
+        mid = (high + low) // 2
+        if arr[mid] < x:
+            low = mid + 1
+        elif arr[mid] > x:
+            high = mid - 1
+        else:
+            return mid
+    return -1
+
+sample_array = [ 2, 3, 4, 10, 40 ]
+target_integer = int(input("target integer >>> "))
+
+result = binary_search(sample_array, target_integer)
+ 
+if result != -1:
+    print("Element index: " + str(result))
+else:
+    print("404 not found.")
